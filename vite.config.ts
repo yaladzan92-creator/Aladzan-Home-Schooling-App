d23e2b997ba9bfd4df6277c5e98a8d2b769576c3
yaladzan92-creator <y.aladzan.92@gmail.com>
2026-06-18 07:59:19 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/Aladzan-Home-Schooling-App/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
